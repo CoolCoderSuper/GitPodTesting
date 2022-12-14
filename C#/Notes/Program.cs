11a67fb184e009a34488d21e8b1ac1a5eb35caf7
Joseph Guenther <76707331+CoolCoderSuper@users.noreply.github.com>
2022-12-14 15:23:43 +0000

    More C#

---
+++ C#/Notes/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+var dataPath = "notes.json";
+while (true){
+    
+}
+
+public class Note
+{
+    public int Id {get;set;}
+
+    public string Value {get;set;}
+
+    public static List<Note> GetNotes()
+    {
+        return new List<Note>();
+    }
+
+    public static void Save()
+    {
+
+    }
+}
