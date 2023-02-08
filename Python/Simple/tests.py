2e861bb3181549b30129898db76bb1382554308b
Joseph Guenther <76707331+CoolCoderSuper@users.noreply.github.com>
2023-02-08 16:36:42 +0000

    Changes

---
+++ Python/Simple/tests.py
@@ -0,0 +1,7 @@
+import random
+import math
+
+c = 0
+while c < 100:
+    print(math.floor(random.random() * 100))
+    c += 1
---
+++ Python/Simple/times.py
@@ -0,0 +1,11 @@
+import time
+
+print("Waiting...")
+time.sleep(1)
+print("Waiting...")
+time.sleep(1)
+print("Waiting...")
+time.sleep(1)
+print("Waiting...")
+time.sleep(1)
+print("Waiting...")
