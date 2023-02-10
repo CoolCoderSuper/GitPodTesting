f42fff25a9e13483df4ddddc714f117c33d391d4
Joseph Guenther <joseph@codingcool.ca>
2023-02-10 19:12:11 +0000

    Changes

--- Java/Notes/Main.java
+++ Java/Notes/Main.java
@@ -7,23 +7,27 @@ public class Main {
         System.out.println("Notes App");
         var objScanner = new Scanner(System.in);
         var cont = true;
-        while (cont){
+        var notes = new ArrayList<Note>();
+        while (cont) {
             System.out.print("> ");
             var command = objScanner.nextLine();
-            switch (command) {
-                case "exit":
-                    cont = false;
-                    break;
-                case "help":
-                    System.out.println("list: lists all the available notes");
-                    System.out.println("add: adds a new note");
-                    System.out.println("remove: removes a note from the list");
-                    System.out.println("exit: exits the program");
-                    System.out.println("help: this list");
-                    break;
-                default:
-                    System.out.println("Invalid command");
-                    break;
+            if (command == "exit") {
+                cont = false;
+            } else if (command == "help") {
+                System.out.println("list: lists all the available notes");
+                System.out.println("add: adds a new note");
+                System.out.println("remove: removes a note from the list");
+                System.out.println("exit: exits the program");
+                System.out.println("help: this list");
+            } else if (command == "list") {
+                System.out.println(notes);
+            } else if (command.startsWith("add")) {
+                var note = command;
+                notes.add(new Note(notes.stream().max(Comparator.comparing(v -> v.Id)).get().Id, note));
+            } else if (command == "remove") {
+
+            } else {
+                System.out.println("Invalid command");
             }
         }
         objScanner.close();
