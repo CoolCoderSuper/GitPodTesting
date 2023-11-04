e9e6c6e03528de22cdcebb746e458ee59a8ab0b3
Joseph Guenther <76707331+CoolCoderSuper@users.noreply.github.com>
2023-11-04 11:59:05 +0000

    Refactor lexer

--- Rust/json_parser/src/lexer.rs
+++ Rust/json_parser/src/lexer.rs
@@ -1,31 +1,35 @@
 use std::ops::Index;
 
 use crate::token::*;
-pub struct Lexer{
+pub struct Lexer {
     text: String,
     position: usize,
-    chars: Vec<char>
+    chars: Vec<char>,
 }
 //TODO: Diagnostics
 //TODO: String escape
 impl Lexer {
-    pub fn new(text: &str) -> Lexer{
-        Lexer { text: text.to_string(), position: 0, chars: text.chars().collect() }
+    pub fn new(text: &str) -> Lexer {
+        Lexer {
+            text: text.to_string(),
+            position: 0,
+            chars: text.chars().collect(),
+        }
     }
 
-    fn current(&self) -> &char{
+    fn current(&self) -> &char {
         self.peek(0)
     }
 
-    fn next(&mut self){
+    fn next(&mut self) {
         self.skip(1)
     }
 
-    fn skip(&mut self, offest: usize){
+    fn skip(&mut self, offest: usize) {
         self.position += offest
     }
 
-    fn peek(&self, offest: usize) -> &char{
+    fn peek(&self, offest: usize) -> &char {
         let index = self.position + offest;
         if index >= self.text.len() {
             return &'\0';
@@ -33,234 +37,308 @@ impl Lexer {
         self.chars.index(index)
     }
 
-    pub fn lex(&mut self) -> SyntaxToken{
-        if self.current() == &'\0'{
+    pub fn lex(&mut self) -> SyntaxToken {
+        if self.matches_eof() {
             return SyntaxToken {
                 text: "".to_string(),
                 position: self.position,
-                kind: SyntaxKind::EndOfFileToken
-            }
+                kind: SyntaxKind::EndOfFileToken,
+            };
         }
-        if self.match_whitespace() {
-            let start = self.position;
-            while self.match_whitespace() {
-                self.next()
-            }
-            return SyntaxToken {
-                text: self.text[start..self.position].to_string(),
-                position: start,
-                kind: SyntaxKind::WhiteSpaceToken
-            }
+        if let Some(token) = self.match_whitespace() {
+            return token;
         }
-        if self.match_openbrace(){
-            self.next();
-            return SyntaxToken {
-                text: '{'.to_string(),
-                position: self.position - 1,
-                kind: SyntaxKind::OpenBraceToken
-            }
+        if let Some(token) = self.match_openbrace() {
+            return token;
         }
-        if self.match_closebrace(){
-            self.next();
-            return SyntaxToken {
-                text: '}'.to_string(),
-                position: self.position - 1,
-                kind: SyntaxKind::CloseBraceToken
-            }
+        if let Some(token) = self.match_closebrace() {
+            return token;
         }
-        if self.match_openbracket(){
-            self.next();
-            return SyntaxToken {
-                text: '['.to_string(),
-                position: self.position - 1,
-                kind: SyntaxKind::OpenBracketToken
-            }
+        if let Some(token) = self.match_openbracket() {
+            return token;
         }
-        if self.match_closebracket(){
-            self.next();
-            return SyntaxToken {
-                text: ']'.to_string(),
-                position: self.position - 1,
-                kind: SyntaxKind::CloseBracketToken
-            }
+        if let Some(token) = self.match_closebracket() {
+            return token;
         }
-        if self.match_comma(){
-            self.next();
-            return SyntaxToken {
-                text: ','.to_string(),
-                position: self.position - 1,
-                kind: SyntaxKind::CommaToken
-            }
+        if let Some(token) = self.match_comma() {
+            return token;
         }
-        if self.match_colon(){
-            self.next();
-            return SyntaxToken {
-                text: ':'.to_string(),
-                position: self.position - 1,
-                kind: SyntaxKind::ColonToken
-            }
+        if let Some(token) = self.match_colon() {
+            return token;
         }
-        if self.match_true(){
-            let start = self.position;
-            self.skip(4);
-            return SyntaxToken {
-                text: "true".to_string(),
-                position: start,
-                kind: SyntaxKind::TrueLiteralToken
-            }
+        if let Some(token) = self.match_true() {
+            return token;
         }
-        if self.match_false(){
-            let start = self.position;
-            self.skip(5);
-            return SyntaxToken {
-                text: "false".to_string(),
-                position: start,
-                kind: SyntaxKind::FalseLiteralToken
-            }
+        if let Some(token) = self.match_false() {
+            return token;
         }
-        if self.match_null(){
-            let start = self.position;
-            self.skip(4);
-            return SyntaxToken {
-                text: "null".to_string(),
-                position: start,
-                kind: SyntaxKind::NullLiteralToken
-            }
+        if let Some(token) = self.match_null() {
+            return token;
         }
-        if self.match_number() {
-            let start = self.position;
-            while self.match_number() {
-                self.next()
-            }
-            return SyntaxToken {
-                text: self.text[start..self.position].to_string(),
-                position: start,
-                kind: SyntaxKind::NumberLiteralToken
-            }
+        if let Some(token) = self.match_number() {
+            return token;
         }
-        if self.match_string(){
-            let start = self.position;
-            self.next();
-            loop {
-                if self.match_string(){
-                    self.next();
-                    break;
-                }else{
-                    self.next();
-                }
-            }
-            return SyntaxToken {
-                text: self.text[start..self.position].to_string(),
-                position: start,
-                kind: SyntaxKind::StringLiteralToken
-            }
+        if let Some(token) = self.match_string() {
+            return token;
         }
-        if self.match_identifier() {
-            let start = self.position;
-            while self.match_identifier() {
-                self.next()
-            }
-            return SyntaxToken {
-                text: self.text[start..self.position].to_string(),
-                position: start,
-                kind: SyntaxKind::IdentifierToken
-            }
+        if let Some(token) = self.match_identifier() {
+            return token;
         }
-        if self.match_comment() {
-            let start = self.position;
-            loop {
-                self.next();
-                if self.current() == &'\r' || self.current() == &'\n' || self.current() == &'\0'{
-                    break;
-                }
-            }
-            return SyntaxToken {
-                text: self.text[start..self.position].to_string(),
-                position: start,
-                kind: SyntaxKind::CommentToken
-            }
+        if let Some(token) = self.match_comment() {
+            return token;
         }
-        if self.match_opencomment() {
-            let start = self.position;
-            self.skip(2);
-            while !self.match_closecomment() && self.current() != &'\0'{
-                self.next();
-            }
-            if self.match_closecomment(){
-                self.skip(2);
-            }
-            return SyntaxToken {
-                text: self.text[start..self.position].to_string(),
-                position: start,
-                kind: SyntaxKind::BlockCommentToken
-            }
+        if let Some(token) = self.match_blockcomment() {
+            return token;
         }
-        let bad = SyntaxToken { text: self.current().to_string(), position: self.position, kind: SyntaxKind::BadToken };        
+        let bad = SyntaxToken {
+            text: self.current().to_string(),
+            position: self.position,
+            kind: SyntaxKind::BadToken,
+        };
         self.next();
         bad
     }
 
-    fn match_whitespace(&self) -> bool{
-        self.current().is_whitespace()
+    fn matches_eof(&self) -> bool {
+        self.current() == &'\0'
     }
 
-    fn match_openbrace(&self) -> bool{
-        self.current() == &'{'
+    fn match_whitespace(&mut self) -> Option<SyntaxToken> {
+        if self.current().is_whitespace() {
+            let start = self.position;
+            while self.current().is_whitespace() {
+                self.next()
+            }
+            return Some(SyntaxToken {
+                text: self.text[start..self.position].to_string(),
+                position: start,
+                kind: SyntaxKind::WhiteSpaceToken,
+            });
+        }
+        None
     }
 
-    fn match_closebrace(&self) -> bool{
-        self.current() == &'}'
+    fn match_openbrace(&mut self) -> Option<SyntaxToken> {
+        if self.current() == &'{' {
+            self.next();
+            return Some(SyntaxToken {
+                text: '{'.to_string(),
+                position: self.position - 1,
+                kind: SyntaxKind::OpenBraceToken,
+            });
+        }
+        None
     }
 
-    fn match_openbracket(&self) -> bool{
-        self.current() == &'['
+    fn match_closebrace(&mut self) -> Option<SyntaxToken> {
+        if self.current() == &'}' {
+            self.next();
+            return Some(SyntaxToken {
+                text: '}'.to_string(),
+                position: self.position - 1,
+                kind: SyntaxKind::CloseBraceToken,
+            });
+        }
+        None
     }
 
-    fn match_closebracket(&self) -> bool{
-        self.current() == &']'
+    fn match_openbracket(&mut self) -> Option<SyntaxToken> {
+        if self.current() == &'[' {
+            self.next();
+            return Some(SyntaxToken {
+                text: '['.to_string(),
+                position: self.position - 1,
+                kind: SyntaxKind::OpenBracketToken,
+            });
+        }
+        None
     }
 
-    fn match_comma(&self) -> bool{
-        self.current() == &','
+    fn match_closebracket(&mut self) -> Option<SyntaxToken> {
+        if self.current() == &']' {
+            self.next();
+            return Some(SyntaxToken {
+                text: ']'.to_string(),
+                position: self.position - 1,
+                kind: SyntaxKind::CloseBracketToken,
+            });
+        }
+        None
     }
 
-    fn match_colon(&self) -> bool{
-        self.current() == &':'
+    fn match_comma(&mut self) -> Option<SyntaxToken> {
+        if self.current() == &',' {
+            self.next();
+            return Some(SyntaxToken {
+                text: ','.to_string(),
+                position: self.position - 1,
+                kind: SyntaxKind::CommaToken,
+            });
+        }
+        None
     }
 
-    fn match_true(&self) -> bool{
-        self.text[self.position..self.position + 4] == "true".to_string()
+    fn match_colon(&mut self) -> Option<SyntaxToken> {
+        if self.current() == &':' {
+            self.next();
+            return Some(SyntaxToken {
+                text: ':'.to_string(),
+                position: self.position - 1,
+                kind: SyntaxKind::ColonToken,
+            });
+        }
+        None
     }
 
-    fn match_false(&self) -> bool{
-        self.text[self.position..self.position + 5] == "false".to_string()
+    fn match_true(&mut self) -> Option<SyntaxToken> {
+        if self.text[self.position..self.position + 4] == "true".to_string() {
+            let start = self.position;
+            self.skip(4);
+            return Some(SyntaxToken {
+                text: "true".to_string(),
+                position: start,
+                kind: SyntaxKind::TrueLiteralToken,
+            });
+        }
+        None
     }
 
-    fn match_null(&self) -> bool{
-        self.text[self.position..self.position + 4] == "null".to_string()
+    fn match_false(&mut self) -> Option<SyntaxToken> {
+        if self.text[self.position..self.position + 5] == "false".to_string() {
+            let start = self.position;
+            self.skip(5);
+            return Some(SyntaxToken {
+                text: "false".to_string(),
+                position: start,
+                kind: SyntaxKind::FalseLiteralToken,
+            });
+        }
+        None
     }
 
-    fn match_number(&self) -> bool{
-        self.current().is_numeric() || self.current() == &'-'|| self.current() == &'+'|| self.current() == &'.' || self.current() == &'e' || self.current() == &'E'
+    fn match_null(&mut self) -> Option<SyntaxToken> {
+        if self.text[self.position..self.position + 4] == "null".to_string() {
+            let start = self.position;
+            self.skip(4);
+            return Some(SyntaxToken {
+                text: "null".to_string(),
+                position: start,
+                kind: SyntaxKind::NullLiteralToken,
+            });
+        }
+        None
     }
 
-    fn match_string(&self) -> bool {
+    fn match_number(&mut self) -> Option<SyntaxToken> {
+        if self.matches_number() {
+            let start = self.position;
+            while self.matches_number() {
+                self.next()
+            }
+            return Some(SyntaxToken {
+                text: self.text[start..self.position].to_string(),
+                position: start,
+                kind: SyntaxKind::NumberLiteralToken,
+            });
+        }
+        None
+    }
+
+    fn matches_number(&self) -> bool {
+        self.current().is_numeric()
+            || self.current() == &'-'
+            || self.current() == &'+'
+            || self.current() == &'.'
+            || self.current() == &'e'
+            || self.current() == &'E'
+    }
+
+    fn match_string(&mut self) -> Option<SyntaxToken> {
+        if self.matches_string() {
+            let start = self.position;
+            self.next();
+            loop {
+                if self.matches_string() {
+                    self.next();
+                    break;
+                } else {
+                    self.next();
+                }
+            }
+            return Some(SyntaxToken {
+                text: self.text[start..self.position].to_string(),
+                position: start,
+                kind: SyntaxKind::StringLiteralToken,
+            });
+        }
+        None
+    }
+
+    fn matches_string(&self) -> bool {
         self.current() == &'"' || self.current() == &'\''
     }
-    
-    fn match_identifier(&self) -> bool{
-        self.current().is_alphanumeric()
+
+    fn match_identifier(&mut self) -> Option<SyntaxToken> {
+        if self.current().is_alphanumeric() {
+            let start = self.position;
+            while self.current().is_alphanumeric() {
+                self.next()
+            }
+            return Some(SyntaxToken {
+                text: self.text[start..self.position].to_string(),
+                position: start,
+                kind: SyntaxKind::IdentifierToken,
+            });
+        }
+        None
     }
 
-    fn match_comment(&self) -> bool{
+    fn match_comment(&mut self) -> Option<SyntaxToken> {
+        if self.matches_comment() {
+            let start = self.position;
+            loop {
+                self.next();
+                if self.current() == &'\r' || self.current() == &'\n' || self.matches_eof() {
+                    break;
+                }
+            }
+            return Some(SyntaxToken {
+                text: self.text[start..self.position].to_string(),
+                position: start,
+                kind: SyntaxKind::CommentToken,
+            });
+        }
+        None
+    }
+
+    fn matches_comment(&self) -> bool {
         self.current() == &'/' && self.peek(1) == &'/'
     }
 
-    fn match_opencomment(&self) -> bool{
+    fn match_blockcomment(&mut self) -> Option<SyntaxToken> {
+        if self.matches_opencomment() {
+            let start = self.position;
+            self.skip(2);
+            while !self.matches_closecomment() && self.matches_eof() {
+                self.next();
+            }
+            if self.matches_closecomment() {
+                self.skip(2);
+            }
+            return Some(SyntaxToken {
+                text: self.text[start..self.position].to_string(),
+                position: start,
+                kind: SyntaxKind::BlockCommentToken,
+            });
+        }
+        None
+    }
+
+    fn matches_opencomment(&self) -> bool {
         self.current() == &'/' && self.peek(1) == &'*'
     }
 
-    fn match_closecomment(&self) -> bool{
+    fn matches_closecomment(&self) -> bool {
         self.current() == &'*' && self.peek(1) == &'/'
     }
 }
